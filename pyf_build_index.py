3973cb8ae2872508970024e9ef69e030eb862132
Lukas Guziel <guziel@interaktiv.de>
2019-02-28 11:45:42 +0100

    first test shot for 100 collective pypi packages

---
+++ pyf_build_index.py
@@ -0,0 +1,18 @@
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+import os
+
+
+client = Elasticsearch([
+	{'host': 'localhost', 'port': '9200'}
+])
+
+for filename in os.listdir('data'):
+	with open('data/' + filename) as file_obj:
+		index_keywords = {
+			'index': 'packages',
+			'doc_type': 'content',
+			'id': filename.replace('.json', ''),
+			'body': file_obj.read()
+		}
+		client.index(**index_keywords)
---
+++ pyf_test_search.py
@@ -0,0 +1,17 @@
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+
+
+client = Elasticsearch([
+	{'host': 'localhost', 'port': '9200'}
+])
+
+search_obj = Search(using=client, index='packages')
+
+search_obj = search_obj.filter(
+	'terms',
+	classifiers=['Framework :: Plone :: 4.2']
+)
+
+result = search_obj.execute()
+print result
