e0ade333c57bacb83de60d3fd44203f8450c9138
Lukas Guziel <guziel@interaktiv.de>
2019-02-28 12:15:22 +0100

    fix intendations

--- pyf_build_index.py
+++ pyf_build_index.py
@@ -1,17 +1,16 @@
-from elasticsearch import Elasticsearch
 import os
-
+from elasticsearch import Elasticsearch
 
 client = Elasticsearch([
-	{'host': 'localhost', 'port': '9200'}
+    {'host': 'localhost', 'port': '9200'}
 ])
 
 for filename in os.listdir('data'):
-	with open('data/' + filename) as file_obj:
-		index_keywords = {
-			'index': 'packages',
-			'doc_type': 'content',
-			'id': filename.replace('.json', ''),
-			'body': file_obj.read()
-		}
-		client.index(**index_keywords)
+    with open('data/' + filename) as file_obj:
+        index_keywords = {
+            'index': 'packages',
+            'doc_type': 'content',
+            'id': filename.replace('.json', ''),
+            'body': file_obj.read()
+        }
+        client.index(**index_keywords)
