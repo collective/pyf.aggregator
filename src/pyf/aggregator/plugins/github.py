b5fb817231d8d46f46c14e00985a99ea742d672e
Paul Grunewald <paul.grunewald@tu-dresden.de>
2020-03-13 11:29:57 +0100

    Add logic for retrieving Github stats

--- src/pyf/aggregator/plugins/github.py
+++ src/pyf/aggregator/plugins/github.py
@@ -1,7 +1,55 @@
-class load_github_stats:
+from github import Github
+from github import RateLimitExceededException
+from pyf.aggregator.logger import logger
 
+import datetime
+import re
+import time
+
+
+PREFIX = "github_"
+github_regex = re.compile(r"^(http[s]{0,1}:\/\/|www\.)github\.com/(.+/.+)")
+
+
+class load_github_stats:
     def __init__(self, settings):
         self.token = settings.get("github_token")
+        self.github = Github(self.token or None)
 
     def __call__(self, identifier, data):
-        return data
+        urls = [data.get("home_page"), data.get("project_url")] + list((data.get("project_urls") or {}).values())
+
+        for url in urls:
+            if not url:
+                continue
+            match = github_regex.match(url)
+            if match:
+                repo_identifier = match.groups()[-1]
+                break
+        else:
+            logger.debug("no github url repository found for {0}".format(identifier))
+            return
+
+        keys_mapping = {
+            "stars": "stargazers_count",
+            "open_issues": "open_issues",
+            "is_archived": "archived",
+            "watchers": "watchers_count",
+            "updated": "updated_at",
+        }
+
+        while True:
+            try:
+                repo = self.github.get_repo(repo_identifier)
+                for key, key_github in keys_mapping.items():
+                    data[PREFIX + key] = getattr(repo, key_github)
+                break
+            except RateLimitExceededException:
+                reset_time = self.github.rate_limiting_resettime
+                delta = reset_time - time.time()
+                logger.info(
+                    "Waiting until {0} (UTC) reset time to perform more Github requests.".format(
+                        datetime.datetime.utcfromtimestamp(reset_time).strftime("%Y-%m-%d %H:%M:%S")
+                    )
+                )
+                time.sleep(delta)
