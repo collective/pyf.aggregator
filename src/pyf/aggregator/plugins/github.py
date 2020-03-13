49fd11f01b4042ef168447a5b1adfe48e2f44693
Paul Grunewald <paul.grunewald@tu-dresden.de>
2020-03-13 14:23:20 +0100

    Improve caching, code structure, docs

--- src/pyf/aggregator/plugins/github.py
+++ src/pyf/aggregator/plugins/github.py
@@ -13,11 +13,13 @@ github_regex = re.compile(r"^(http[s]{0,1}:\/\/|www\.)github\.com/(.+/.+)")
 
 
 def memoize(obj):
+    """Decorator for memoizing the return value.
+    """
     cache = obj.cache = {}
 
     @functools.wraps(obj)
     def memoizer(*args, **kwargs):
-        key = args[1].split("-")[0]
+        key = args[1]
         if key not in cache:
             cache[key] = obj(*args, **kwargs)
         return cache[key]
@@ -25,26 +27,18 @@ def memoize(obj):
     return memoizer
 
 
-class load_github_stats:
+class GithubStats:
+    """Helper to retrieve Github data.
+
+    """
     def __init__(self, settings):
         self.token = settings.get("github_token")
         self.github = Github(self.token or None)
 
     @memoize
-    def __call__(self, identifier, data):
-        urls = [data.get("home_page"), data.get("project_url")] + list((data.get("project_urls") or {}).values())
-
-        for url in urls:
-            if not url:
-                continue
-            match = github_regex.match(url)
-            if match:
-                repo_identifier = match.groups()[-1]
-                break
-        else:
-            logger.debug("no github url repository found for {0}".format(identifier))
-            return
-
+    def add_github_data(self, repo_identifier, data):
+        """Add stats from a given Github repository (e.g. MyCompany/my_repo) to data.
+        """
         keys_mapping = {
             "stars": "stargazers_count",
             "open_issues": "open_issues",
@@ -52,7 +46,6 @@ class load_github_stats:
             "watchers": "watchers_count",
             "updated": "updated_at",
         }
-
         while True:
             try:
                 repo = self.github.get_repo(repo_identifier)
@@ -68,3 +61,28 @@ class load_github_stats:
                     )
                 )
                 time.sleep(delta)
+
+    def __call__(self, identifier, data):
+        """Search for a referenced Github repository from pypi package information and if present, add those relevant
+        Github stats.
+        """
+        urls = [data.get("home_page"), data.get("project_url")] + list((data.get("project_urls") or {}).values())
+
+        for url in urls:
+            if not url:
+                continue
+            match = github_regex.match(url)
+            if match:
+                repo_identifier = match.groups()[-1]
+                break
+        else:
+            logger.debug("no github url repository found for {0}".format(identifier))
+            return
+
+        self.add_github_data(repo_identifier, data)
+
+
+def load_github_stats(settings):
+    """Return a callable to add Github stats.
+    """
+    return GithubStats(settings)
