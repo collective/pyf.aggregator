df4eb7f67768ae880ded6fd6692143c85d17308c
MrTango <md@derico.de>
2022-11-22 23:39:16 +0200

    catch exceptions in RST2HTML convertion

--- src/pyf/aggregator/plugins/rst_to_html.py
+++ src/pyf/aggregator/plugins/rst_to_html.py
@@ -1,16 +1,21 @@
-from pkg_resources import parse_version
-
 from docutils import core
+from pkg_resources import parse_version
+from pyf.aggregator.logger import logger
+
 import re
 
 
 def process(identifier, data):
     """convert package description from RST to HTML"""
-    data["description"] = core.publish_parts(
-        data["description"],
-        writer_name="html",
-        settings_overrides={"initial_header_level": 2, "doctitle": False},
-    )["html_body"]
+    try:
+        data["description"] = core.publish_parts(
+            data["description"],
+            writer_name="html",
+            settings_overrides={"initial_header_level": 2, "doctitle": False},
+        )["html_body"]
+    except Exception as e:
+        logger.warn(f"Could not convert RST to HTML for {data['name']}: \n\n{e}")
+
 
 
 def load(settings):
