1790f42039b61fe4e6415d04b1610045733908ce
MrTango <md@derico.de>
2023-02-08 19:06:43 +0100

    fix plugin registry

--- src/pyf/aggregator/plugins/__init__.py
+++ src/pyf/aggregator/plugins/__init__.py
@@ -9,7 +9,7 @@ from . import rst_to_html
 def register_plugins(PLUGINS, settings):
     # PLUGINS.append(load_curated(settings))
     PLUGINS.append(version_slicer.load(settings))
-    PLUGINS.append(version_timestamp.load(settings))
+    # PLUGINS.append(version_timestamp.load(settings))
     PLUGINS.append(framwork_versions.load(settings))
     PLUGINS.append(python_versions.load(settings))
     PLUGINS.append(rst_to_html.load(settings))
